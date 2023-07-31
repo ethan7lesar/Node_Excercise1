c4cfe49f0385b84fc4e9aec49de46eba657e7eba
ethan7lesar <ethanlesar11@gmail.com>
2023-07-31 14:13:18 +0200

    Good job

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var fs = require("fs");
 
-fs.readFile('data.txt', function (err, data) {
-   if (err) return console.error(err);
-   console.log(data.toString());
+
+var fs = require('fs');
+
+fs.appendFile('data.txt', 'I did it wrong but its fine!', function (err, data) { 
+                        if (err)
+        console.log(err);
+                        else
+        console.log('Append operation complete.');
+       
 });
 
-// It placed it on the next line when the data.txt file was appended 
+// it will add to last statement statement.
+
+
